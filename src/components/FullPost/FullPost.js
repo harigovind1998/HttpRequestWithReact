8877449d24792011dec2cdc05271d8ddea754b68
Hari Govind <grhari1998@gmail.com>
2020-08-11 21:20:28 -0400

    retreives the selected dummy post

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,22 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './FullPost.css';
+import "./FullPost.css";
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+  state = {
+    postTitle: "Title",
+    postBody: "Body",
+  };
 
-        );
-        return post;
+  componentDidUpdate(prevProps) {
+    console.log("component did mount");
+    if (this.props.id && this.props.id !== prevProps.id) {
+      const getUrl =
+        "https://jsonplaceholder.typicode.com/posts/" + this.props.id;
+      axios.get(getUrl).then((response) => {
+        this.setState({
+          postTitle: response.data.title,
+          postBody: response.data.body,
+        });
+      });
     }
+  }
+
+  render() {
+    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.postTitle}</h1>
+          <p>{this.state.postBody}</p>
+          <div className="Edit">
+            <button className="Delete">Delete</button>
+          </div>
+        </div>
+      );
+    }
+
+    return post;
+  }
 }
 
 export default FullPost;
